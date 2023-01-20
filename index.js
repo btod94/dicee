20804a3bbfa8bba417397b2d229a563bde1a2ea1
btod94 <122179842+btod94@users.noreply.github.com>
2023-01-20 20:59:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// Coding the dices
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var image1 = "images/dice"+randomNumber1+".png";
+var image2 = "images/dice"+randomNumber2+".png";
+document.querySelector(".dice .img1").setAttribute("src",image1);
+document.querySelector(".dice .img2").setAttribute("src",image2);
+// document.querySelectorAll("img")[0].setAttribute("src",image1);
+
+// Displaying winner
+
+if(randomNumber1>randomNumber2){
+    document.querySelector(".container h1").textContent="Player 1 Wins!";
+}
+
+else if(randomNumber1<randomNumber2){
+    document.querySelector(".container h1").textContent="Player 2 Wins!";
+}
+
+else{
+    document.querySelector(".container h1").textContent="It's a draw!";
+}
